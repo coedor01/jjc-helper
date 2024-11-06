cd19bcfec91ae3bc0a0664d74b3472e237b9a764
coedor01 <135929935+coedor01@users.noreply.github.com>
2024-11-06 20:22:06 +0800

    feat: 修改房间UI

--- src/app/rooms/[id]/components/Body.tsx
+++ src/app/rooms/[id]/components/Body.tsx
@@ -3,7 +3,7 @@
 export default function Body() {
   return (
     <>
-      <div className="w-full h-12 bg-sky-600 inline-flex justify-center items-center text-white">
+      <div className="w-full h-12 bg-sky-700 inline-flex justify-center items-center text-white">
         房间
       </div>
       <div className="fixed top-14 bottom-0 w-full flex flex-col">
@@ -15,7 +15,7 @@ export default function Body() {
               </div>
             </div>
             <div className="relative size-full flex flex-col">
-              <span className="text-right bg-gradient-to-r from-sky-50 to-sky-600 flex items-center justify-between">
+              <span className="text-right bg-gradient-to-r from-sky-50 to-sky-700 flex items-center justify-between">
                 <span className="pl-1">我·唯我独尊</span>
                 <span className="pr-1 text-white">14段</span>
               </span>
@@ -31,7 +31,7 @@ export default function Body() {
               </div>
             </div>
             <div className="relative size-full flex flex-col">
-              <span className="text-right bg-gradient-to-r from-sky-50 to-sky-600 flex items-center justify-between">
+              <span className="text-right bg-gradient-to-r from-sky-50 to-sky-700 flex items-center justify-between">
                 <span className="pl-1">我·唯我独尊</span>
                 <span className="pr-1 text-white">14段</span>
               </span>
@@ -47,7 +47,7 @@ export default function Body() {
               </div>
             </div>
             <div className="relative size-full flex flex-col">
-              <span className="text-right bg-gradient-to-r from-sky-50 to-sky-600 flex items-center justify-between">
+              <span className="text-right bg-gradient-to-r from-sky-50 to-sky-700 flex items-center justify-between">
                 <span className="pl-1">我·唯我独尊</span>
                 <span className="pr-1 text-white">14段</span>
               </span>
@@ -63,7 +63,7 @@ export default function Body() {
               </div>
             </div>
             <div className="relative size-full flex flex-col">
-              <span className="text-right bg-gradient-to-r from-sky-50 to-sky-600 flex items-center justify-between">
+              <span className="text-right bg-gradient-to-r from-sky-50 to-sky-700 flex items-center justify-between">
                 <span className="pl-1">我·唯我独尊</span>
                 <span className="pr-1 text-white">14段</span>
               </span>
@@ -79,7 +79,7 @@ export default function Body() {
               </div>
             </div>
             <div className="relative size-full flex flex-col">
-              <span className="text-right bg-gradient-to-r from-sky-50 to-sky-600 flex items-center justify-between">
+              <span className="text-right bg-gradient-to-r from-sky-50 to-sky-700 flex items-center justify-between">
                 <span className="pl-1">我·唯我独尊</span>
                 <span className="pr-1 text-white">14段</span>
               </span>
@@ -95,28 +95,73 @@ export default function Body() {
             房间还剩 05:00 消失
           </div>
         </div>
-        <div className="flex-1 bg-sky-600 rounded-md text-white p-3 overflow-y-auto overscroll-y-contain m-2">
-          <div>张三：hello 大家好</div>
-          <div>张三：我们今天去哪里玩</div>
-          <div>李四：要不去爬山吧</div>
-          <div>王五：我不想出门</div>
-          <div>赵六：看明天天气怎么样吧</div>
-          <div>张三：hello 大家好</div>
-          <div>张三：我们今天去哪里玩</div>
-          <div>李四：要不去爬山吧</div>
-          <div>王五：我不想出门</div>
-          <div>赵六：看明天天气怎么样吧</div>
-          <div>张三：hello 大家好</div>
-          <div>张三：我们今天去哪里玩</div>
-          <div>李四：要不去爬山吧</div>
-          <div>王五：我不想出门</div>
-          <div>赵六：看明天天气怎么样吧</div>
+        <div className="flex-1 bg-sky-50 rounded-md text-sky-800 p-3 overflow-y-auto overscroll-y-contain m-2">
+          <div>
+            <span className="font-semibold">张三：</span>
+            <span>hello 大家好</span>
+          </div>
+          <div>
+            <span className="font-semibold">张三：</span>
+            <span>我们今天去哪里玩</span>
+          </div>
+          <div>
+            <span className="font-semibold">李四：</span>
+            <span>要不去爬山吧</span>
+          </div>
+          <div>
+            <span className="font-semibold">王五：</span>
+            <span>我不想出门</span>
+          </div>
+          <div>
+            <span className="font-semibold">赵六：</span>
+            <span>看明天天气怎么样吧</span>
+          </div>
+          <div>
+            <span className="font-semibold">张三：</span>
+            <span>hello 大家好</span>
+          </div>
+          <div>
+            <span className="font-semibold">张三：</span>
+            <span>我们今天去哪里玩</span>
+          </div>
+          <div>
+            <span className="font-semibold">李四：</span>
+            <span>要不去爬山吧</span>
+          </div>
+          <div>
+            <span className="font-semibold">王五：</span>
+            <span>我不想出门</span>
+          </div>
+          <div>
+            <span className="font-semibold">赵六：</span>
+            <span>看明天天气怎么样吧</span>
+          </div>
+          <div>
+            <span className="font-semibold">张三：</span>
+            <span>hello 大家好</span>
+          </div>
+          <div>
+            <span className="font-semibold">张三：</span>
+            <span>我们今天去哪里玩</span>
+          </div>
+          <div>
+            <span className="font-semibold">李四：</span>
+            <span>要不去爬山吧</span>
+          </div>
+          <div>
+            <span className="font-semibold">王五：</span>
+            <span>我不想出门</span>
+          </div>
+          <div>
+            <span className="font-semibold">赵六：</span>
+            <span>看明天天气怎么样吧</span>
+          </div>
         </div>
-        <div className="w-full h-16 bg-sky-600 rounded-t text-white inline-flex items-center pl-2 pr-2 ">
+        <div className="w-full h-16 bg-sky-700 rounded-t text-white inline-flex items-center pl-2 pr-2 ">
           <input
             type="text"
             placeholder="请输入要发送的文本"
-            className="flex-1 input  text-black"
+            className="flex-1 input  text-black input-ghost bg-sky-50 active:outline-none"
           />
         </div>
       </div>
